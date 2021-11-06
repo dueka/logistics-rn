a2ef016583f128b3a20c6104dc6938ce534a4774
Ekanem <ekanemd@live.com>
2021-11-06 08:51:38 +0100

    updated styles on login

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Box } from ".";
+import { Image, Dimensions, StyleSheet, StatusBar } from "react-native";
+import { Box, theme } from ".";
 
+export const assets = [require("./assets/patterns/1.jpg")];
+const { width } = Dimensions.get("window");
+const aspectRatio = 750 / 1125;
+const height = width * aspectRatio;
 interface ContainerProps {
   children: ReactNode;
 }
-
-const Container = () => {
+const Container = ({ children }: ContainerProps) => {
   return (
-    <Box flex={1}>
-      <Box>{/* <Image />  */}</Box>
+    <Box flex={1} backgroundColor="primary">
+      <StatusBar barStyle="light-content" />
+      <Box borderBottomLeftRadius="xl" overflow="hidden" height={height * 0.61}>
+        <Image
+          source={assets[0]}
+          style={{
+            width,
+            height,
+            borderBottomLeftRadius: theme.borderRadii.xl,
+          }}
+        />
+      </Box>
+      <Box flex={1} overflow="hidden">
+        <Image
+          source={assets[0]}
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            width,
+            height,
+            top: -height * 0.61,
+          }}
+        />
+        <Box
+          borderRadius="xl"
+          borderTopLeftRadius={0}
+          backgroundColor="white"
+          flex={1}
+        >
+          {children}
+        </Box>
+      </Box>
+      <Box height={200} backgroundColor="primary"></Box>
     </Box>
   );
 };
